598ff2a09897eb544604b87474f81d1133c4a631
krambri <Janniklas1010@googlemail.com>
2013-06-19 20:52:38 +0300

    Update methods.java

--- methods.java
+++ methods.java
@@ -21,15 +21,8 @@ public class methods{
 	
 	
 	
-	public methods(Coins instance){
-		this.plugin = instance;
-		host = plugin.getConfig().getString("MySQL.host"); 
-		port = plugin.getConfig().getString("MySQL.port");
-		db = plugin.getConfig().getString("MySQL.database");
-		user = plugin.getConfig().getString("MySQL.user");
-		pw = plugin.getConfig().getString("MySQL.password");
-		sql = new MySQL(Logger.getLogger("Minecraft"),"[KrambriChat]", host, port, db, user, pw);
-		sql.open();
+	public methods(){
+		
 	}
 	
 
@@ -52,7 +45,7 @@ public class methods{
 	}
 	
 	
-	public boolean setCoins(String Playername, int coins){
+	public boolean addCoins(String Playername, int coins){
 		String query = "SELECT `Coins` FROM `coins` WHERE `Player` = '" + Playername + "' LIMIT 0, 30 ";
 		ResultSet rs;
 		int rss = 0;
@@ -117,6 +110,42 @@ public class methods{
 	}
 	
 	
+	public boolean removeCoins(String Playername, int coins){
+		String query = "SELECT `Coins` FROM `coins` WHERE `Player` = '" + Playername + "' LIMIT 0, 30 ";
+		ResultSet rs;
+		int rss = 0;
+		try {
+			rs = sql.query(query);
+			while (rs.next()) {
+				rss = Integer.getInteger(rs.getString("Coins"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		int erg = rss - coins;
+		
+		query = "UPDATE `coins` SET `Coins`= " + erg + " WHERE `Player` = '" + Playername + "'";
+		try {
+			sql.query(query);
+		} catch (SQLException e) {
+			return false;
+		}
+		return true;
+
+	}
+	
+	public void instantiat(Coins instance){
+		this.plugin = instance;
+		host = plugin.getConfig().getString("MySQL.host"); 
+		port = plugin.getConfig().getString("MySQL.port");
+		db = plugin.getConfig().getString("MySQL.database");
+		user = plugin.getConfig().getString("MySQL.user");
+		pw = plugin.getConfig().getString("MySQL.password");
+		sql = new MySQL(Logger.getLogger("Minecraft"),"[KrambriChat]", host, port, db, user, pw);
+		sql.open();
+	}
 	
 	
 }
